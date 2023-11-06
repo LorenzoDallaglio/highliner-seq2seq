4d290793468857382daf83842e6c680ba193df15
Lorenzo Dall'Aglio <10608226@polimi.it>
2023-11-06 15:57:37 +0100

    Fixed bug regarding parsing of inline token as regex

--- modules/tokenizer.py
+++ modules/tokenizer.py
@@ -2,13 +2,13 @@ import re
 
 def tokenize(raw_list, inline_token):
     instruction_list = []
-    header_regex = r"^ *[0-9a-f]*:"
+    header_regex = r"^0x[0-9a-f]*:"
     bytes_regex = r"^ +([0-9a-f]{2} )+ +" #currently useless
     operators_regex = r"([\[\]\+\-\*:])"
     long_address_regex = r"0x[0-9a-f]{5,}"
     trailing_chars_regex = r" *(#.+)*[\n\r]"
     for raw_instruction in raw_list:
-        clean_instruction = re.sub(inline_token, '', raw_instruction)
+        clean_instruction = raw_instruction.replace(inline_token, '')
         clean_instruction = re.sub(header_regex, '', clean_instruction)
         clean_instruction = re.sub(bytes_regex, '', clean_instruction)
         clean_instruction = re.sub(r",", ' ', clean_instruction)
